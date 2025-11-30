369b1e08c1a23ae764ad9fc03997fde0ece999d8
minan1987 <mina.nazary66@gmail.com>
2025-11-30 16:36:18 +0330

    feat: accreditations section was implemented

--- src/components/ClientsContact.tsx
+++ src/components/ClientsContact.tsx
@@ -1,10 +1,10 @@
 import ContactFrom from "./ContactFrom";
-import img1 from "/images/icon6.jpg";
-import img2 from "/images/icon5.jpg";
-import img3 from "/images/icon4.jpg";
-import img4 from "/images/icon9.jpg";
-import img5 from "/images/icon8.jpg";
-import img6 from "/images/icon7.jpg";
+import img1 from "/images/accreditations/acc1.jpg";
+import img2 from "/images/accreditations/acc2.png";
+import img3 from "/images/accreditations/acc3.jpg";
+import img4 from "/images/accreditations/acc4.jpg";
+import img5 from "/images/accreditations/acc5.jpg";
+import img6 from "/images/accreditations/acc6.jpg";
 
 const ClientsContact = () => {
   return (
@@ -14,25 +14,49 @@ const ClientsContact = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           {/* LEFT - Clients */}
           <div className="clients lg:pr-10">
-            <div className="title mb-4">
+            <div className="title mb-8">
               <h2 className="text-lg text-gray-600">WE WORK GLOBALLY</h2>
-              <h3 className="text-4xl font-bold">Our Clients</h3>
+              <h3 className="text-4xl font-bold">Our Accreditations</h3>
             </div>
-
+            {/* 
             <p className="text-gray-500 mb-6">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
               consequuntur voluptate eaque dolor officia possimus iusto iste,
               sed molestias officiis accusantium neque pariatur!
-            </p>
+            </p> */}
 
             {/* Clients Logos Grid */}
             <div className="grid grid-cols-3 gap-4">
-              <img src={img1} className="w-full" alt="" />
-              <img src={img2} className="w-full" alt="" />
-              <img src={img3} className="w-full" alt="" />
-              <img src={img4} className="w-full" alt="" />
-              <img src={img5} className="w-full" alt="" />
-              <img src={img6} className="w-full" alt="" />
+              <img
+                src={img1}
+                className="w-full border border-gray-200"
+                alt=""
+              />
+              <img
+                src={img2}
+                className="w-full border border-gray-200"
+                alt=""
+              />
+              <img
+                src={img3}
+                className="w-full border border-gray-200"
+                alt=""
+              />
+              <img
+                src={img4}
+                className="w-full border border-gray-200"
+                alt=""
+              />
+              <img
+                src={img5}
+                className="w-full border border-gray-200"
+                alt=""
+              />
+              <img
+                src={img6}
+                className="w-full border border-gray-200"
+                alt=""
+              />
             </div>
           </div>
 
